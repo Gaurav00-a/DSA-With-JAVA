3f5ad75990a9f6f56fb368052c0370e245ecc458
Gaurav Yadav <grvydv5008@gmail.com>
2025-04-09 17:11:49 +0530

    Completed Day 1

---
+++ Arrays/2d arrays/aman.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class aman {
+    public static void search(int matrix[][]) {
+        int k=matrix[0][0];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (k<matrix[i][j]) {
+                    k=matrix[i][j];
+                }
+            }
+        }
+        System.out.println("This is greatest : "+k);
+    }
+
+    public static void main(String[] args) {
+        int matrix[][] = new int[3][3];
+
+        int n = matrix.length, m = matrix[0].length;
+
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+
+                matrix[i][j] = sc.nextInt();
+            }
+
+        }
+
+        // output
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(matrix[i][j] + "  ");
+
+            }
+            System.out.println();
+        }
+        search(matrix);
+    }
+
+}
