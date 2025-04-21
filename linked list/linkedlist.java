f3b91f1c7e2fb4dd8d3d06dbb7f20470cdb86be7
Gaurav Yadav <grvydv5008@gmail.com>
2025-04-21 23:14:04 +0530

    Add files via upload
    linked list

---
+++ linked list/linkedlist.java
@@ -0,0 +1,100 @@
+public class linkedlist {
+
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+    public void addFirst(int data) {
+
+        // Creating new node
+        Node newNode = new Node(data);
+        size++;
+
+        if (head == null) {
+            head = tail = newNode;
+            return;
+
+        }
+        // Step 2
+        newNode.next = head; // LINK
+
+        // STEP 3
+        head = newNode;
+
+    }
+
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        size++;
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+
+        // step 2
+        tail.next = newNode;
+
+        // step 3
+        tail = newNode;
+    }
+
+    public void print() {
+        if (head == null) {
+            System.out.println("ll is empty");
+            return;
+        }
+        Node temp = head;
+
+        while (temp != null) {
+            System.out.print(temp.data + " < ");
+            temp = temp.next;
+
+        }
+        System.out.println();
+    }
+
+    public void add(int index, int data) {
+        if (index == 0) {
+            addFirst(data);
+            return;
+
+        }
+        Node newNode = new Node(data);
+        size++;
+        Node temp = head;
+        int i = 0;
+        while (i < index - 1) {
+            temp = temp.next;
+            i++;
+        }
+
+        // i=index-1; temp=previous;
+        newNode.next = temp.next;
+        temp.next = newNode;
+    }
+
+    public static void main(String[] args) {
+        linkedlist ll = new linkedlist();
+        ll.addFirst(2);
+
+        ll.addFirst(1);
+
+        ll.addLast(3);
+
+        ll.addLast(4);
+        ll.add(2, 9);
+        ll.print(); // 1->2->9->4->5
+        System.out.println(ll.size);
+
+    }
+}
